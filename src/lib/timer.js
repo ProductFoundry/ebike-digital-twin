42285b549d9e90c7ee29b8f9ca34dab2f3abd9ef
Arunadevi <l.arunadevi@gmail.com>
2023-01-23 15:29:04 +0530

    Generated log file

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -1,33 +1,50 @@
 function startTimer(seconds, container) {
-    let startTime, timer, obj, ms = seconds*1000,
+    let startTime, timer, obj, ms = seconds * 1000,
         display = document.getElementById(container);
     obj = {};
-    obj.resume = function() {
+    obj.resume = function () {
         startTime = new Date().getTime();
-        timer = setInterval(obj.step,100); // adjust this number to affect granularity
-                            // lower numbers are more accurate, but more CPU-expensive
+        timer = setInterval(obj.step, 1000); // adjust this number to affect granularity
+        // lower numbers are more accurate, but more CPU-expensive
     };
-    obj.pause = function() {
+    obj.pause = function () {
         ms = obj.step();
         clearInterval(timer);
     };
-    obj.stop = function() {
-        ms = seconds*1000;
+    obj.stop = function () {
+        ms = seconds * 1000;
         clearInterval(timer);
         display.innerHTML = "0:0:0";
     };
-    obj.step = function() {
-        let now = Math.max(0,ms+(new Date().getTime()-startTime)),
-            m = Math.floor(now/60000), s = Math.floor(now/1000)%60, mis = Math.floor(now%1000);
-        s = (s < 10 ? "0" : "")+s;
-        display.innerHTML = m+":"+s+":"+mis;
-        if( now == 0) {
+    obj.step = function () {
+        let now = Math.max(0, ms + (new Date().getTime() - startTime)),
+            m = Math.floor(now / 60000), s = Math.floor(now / 1000) % 60, mis = Math.floor(now % 1000);
+        s = (s < 10 ? "0" : "") + s;
+        // display.innerHTML = m + ":" + s + ":" + mis;
+        display.innerHTML = m + ":" + s;
+        if (now == 0) {
             clearInterval(timer);
-            obj.resume = function() {};
+            obj.resume = function () { };
             // if( oncomplete) oncomplete();
         }
         const right = $(".right")[0];
-        $(right).append("<div>"+m+":"+s+":"+mis+" - {ss: 0, sfg: 0, srg: 0, ssg: 0, cad: 0, tor:0, rpm: 0, curr: 0}</div>")
+        const ss = $("input:radio[name ='btnradioss']:checked").val();
+        const sfg = $("input:radio[name ='btnradiofg']:checked").val();
+        const srg = $("input:radio[name ='btnradiorg']:checked").val();
+        const ssg = $("input:radio[name ='btnradiosg']:checked").val();
+        const cad = $("#cadenceSensor input")[0].value;
+        const tor = $("#torqueSensor input")[0].value;
+        const rpm = $("#rpmSensor input")[0].value;
+        const curr = $("#currentSensor input")[0].value;
+
+
+        // $(right).append("<div>" +
+        //     "{time: " + m + ":" + s + ":" + mis + "ss: 0, sfg: 0, srg: 0, ssg: 0, cad: " + cad + ", tor: " + tor + ", rpm: " + rpm + ", curr: " + curr + "}</div>")
+        $(right).append("<div>" +
+            m + ":" + s + "," + ss + " ," +
+            // sfg + " ," + srg + ", " + ssg + "," +
+            cad + "," + tor + "," + rpm + "," + curr + "</div>")
+
         return now;
     };
     obj.resume();
